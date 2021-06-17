13f833f85caf72d2fb58056cc7e3db3383725d79
feiyun0112 <feiyun0112@gmail.com>
2021-06-17 23:40:52 +0800

    什么？他居然想在DLL中放毒！

---
+++ 2021/20210617/ConsoleApp1/ClassLibrary1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public void Test()
+        {
+            Console.WriteLine("执行Test方法");
+        }
+    }
+    class Class2
+    {
+        [ModuleInitializer]
+        internal static void Hack()
+        {
+            Console.WriteLine("执行恶意代码！");
+        }
+    }
+}
---
+++ 2021/20210617/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("公众号“My IO”");
+            new Class1().Test();
+        }
+    }
+}
