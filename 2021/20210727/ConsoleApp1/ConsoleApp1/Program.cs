b6f5193f55a273887d2e87295eb2d0ae78e89447
feiyun0112 <feiyun0112@gmail.com>
2021-08-01 19:18:02 +0800

    20210727

---
+++ 2021/20210727/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        new Program().TestLock();
+
+        Console.Read();
+    }
+
+    void TestLock()
+    {
+        lock (this)
+        {
+            var task = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("-------开始-------");
+                Deadlock();
+                Console.WriteLine("---------完成--------");
+            });
+
+            task.Wait();
+        }
+    }
+
+    void Deadlock()
+    {
+        lock (this)
+        {
+            Console.WriteLine("公众号“My IO”");
+        }
+    }
+}
---
+++ 2021/20210727/README.md
@@ -0,0 +1,1 @@
+杀鸡焉用牛刀！放下Windbg，让dotnet-stack来快速定位死锁原因
