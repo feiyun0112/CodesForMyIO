4d60ae3bf4a81f0519aa278c4c626aeb1fb6a2e7
feiyun0112 <feiyun0112@gmail.com>
2021-08-01 19:26:58 +0800

    20210731

---
+++ 2021/20210731/WebApplication7/WebApplication7/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication7.Dtos;
+using WebApplication7.Entities;
+using AutoMapperGenerator;
+namespace WebApplication7.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [AutoMapping(typeof(UserEntity), typeof(UserDto))]
+    public class UserController : ControllerBase
+    { 
+        [HttpGet]
+        public UserDto  Get(int id)
+        {
+            var userEntity = GetFromDB(id);
+            var userDto = userEntity.ToUserDto();
+
+            return userDto;
+        }
+
+        private UserEntity GetFromDB(int id)
+        {
+            return new UserEntity { Id = id, Name ="My IO" };
+        }
+    }
+}
---
+++ 2021/20210731/WebApplication7/WebApplication7/Dtos/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication7.Dtos
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
