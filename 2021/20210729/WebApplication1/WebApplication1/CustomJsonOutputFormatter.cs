f9c348d8d8bc0e9a280c100e9732ba3a4977fe9e
feiyun0112 <feiyun0112@gmail.com>
2021-08-01 19:22:20 +0800

    20210729

---
+++ 2021/20210729/WebApplication1/WebApplication1/CustomJsonOutputFormatter.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class CustomJsonOutputFormatter : Microsoft.AspNetCore.Mvc.Formatters.SystemTextJsonOutputFormatter
+    {
+        private readonly SystemTextJsonOutputFormatter pascalCaseFormater;
+        public CustomJsonOutputFormatter(JsonSerializerOptions jsonSerializerOptions) : base(jsonSerializerOptions)
+        {
+            var newOptions = new JsonSerializerOptions(jsonSerializerOptions);
+            newOptions.PropertyNamingPolicy = null;
+            pascalCaseFormater = new SystemTextJsonOutputFormatter(newOptions);
+        }
+
+        public override Task WriteAsync(OutputFormatterWriteContext context)
+        {
+            if (GetFormat(context) == "json2")
+            {
+                return pascalCaseFormater.WriteAsync(context);
+            }
+            return base.WriteAsync(context);
+        }
+
+        private string? GetFormat(OutputFormatterWriteContext context)
+        {
+            if (context.HttpContext.Request.RouteValues.TryGetValue("format", out var obj))
+            {
+                var routeValue = Convert.ToString(obj, CultureInfo.InvariantCulture);
+                return string.IsNullOrEmpty(routeValue) ? null : routeValue;
+            }
+
+            var query = context.HttpContext.Request.Query["format"];
+            if (query.Count > 0)
+            {
+                return query.ToString();
+            }
+
+            return "json";
+        }
+    }
+}
---
+++ 2021/20210729/WebApplication1/WebApplication1/WeatherForecast.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace WebApplication1
+{
+    public class WeatherForecast
+    {
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
