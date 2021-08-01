88c5278a59ff9fe759872f2996845312be5456ae
feiyun0112 <feiyun0112@gmail.com>
2021-08-01 19:14:23 +0800

    20210722

---
+++ 2021/20210722/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using Serilog;
+using Serilog.Events;
+using Serilog.Templates;
+using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console(new ExpressionTemplate("{#each name, value in @p}   {name} = {value}\n{#end}"))
+                .CreateLogger();
+
+
+            var user = new { Name = "张三", Password = "xxx" };
+            Log.Information("登录信息：{@Name} {@Password}", user.Name,user.Password);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
